f5b9a4c5d3daa919ca2e48fc4197feaa24b66d2b
Thanakit Janlong <664259007@webmail.npru.ac.th>
2025-10-05 14:17:57 +0700

    f_vite_vercel

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
+  root: './client',
   plugins: [react()],
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
   server: {
     port: 5173,
-    open: false
-  }
+  },
 })
